9c7e0b93e91009f87aa993f6d7777d42f9c1e574
Petr Vojacek <pvojacek@foxconndrc.com>
2019-02-12 18:23:04 +0100

    extended test data

--- server_test.sh
+++ server_test.sh
@@ -1,70 +1,66 @@
 #!/bin/bash
 
-#create user
+HOST=${1:-"localhost:8000"}
+echo "Using host '${HOST}'"
+
+#create users
+for i in {1..500}
+do
 curl -X PUT \
-  http://localhost:8000/api/user \
+  http://${HOST}/api/user \
   -H 'Content-Type: application/json' \
   -H 'cache-control: no-cache' \
   -d '{
-   "email" : "jdoe@email.com",
+   "email" : "jdoe_'$i'@email.com",
    "firstname" : "John",
    "lastname" : "Doe",
    "password" : "a"
 }'
+done
 
 #get user info
 curl -X GET \
-  http://localhost:8000/api/user \
-  -H 'Authorization: Basic amRvZUBlbWFpbC5jb206YQ==' \
+  http://${HOST}/api/user \
+  -H 'Authorization: Basic amRvZV8xQGVtYWlsLmNvbTph' \
   -H 'cache-control: no-cache'
 
-#create team
+#create teams
+for i in {1..500}
+do
 curl -X PUT \
-  'http://localhost:8000/api/team?email=pvojacek@foxconndrc.com' \
-  -H 'Authorization: Basic amRvZUBlbWFpbC5jb206YQ==' \
+  "http://${HOST}/api/team" \
+  -H 'Authorization: Basic amRvZV8xQGVtYWlsLmNvbTph' \
   -H 'Content-Type: application/json' \
   -H 'cache-control: no-cache' \
   -d '{
-	"name" : "new_team",
+	"name" : "new_team_'${i}'",
 	"Description" : "brand new team"
 }'
+done
 
-#create team
-curl -X PUT \
-  'http://localhost:8000/api/team?email=pvojacek@foxconndrc.com' \
-  -H 'Authorization: Basic amRvZUBlbWFpbC5jb206YQ==' \
-  -H 'Content-Type: application/json' \
-  -H 'cache-control: no-cache' \
-  -d '{
-	"name" : "new_team_1",
-	"Description" : "brand new team"
-}'
-
-#create team
-curl -X PUT \
-  'http://localhost:8000/api/team?email=pvojacek@foxconndrc.com' \
-  -H 'Authorization: Basic amRvZUBlbWFpbC5jb206YQ==' \
-  -H 'Content-Type: application/json' \
-  -H 'cache-control: no-cache' \
-  -d '{
-	"name" : "new_team_2",
-	"Description" : "brand new team"
-}'
-
-#create team
-curl -X PUT \
-  'http://localhost:8000/api/team?email=pvojacek@foxconndrc.com' \
-  -H 'Authorization: Basic amRvZUBlbWFpbC5jb206YQ==' \
-  -H 'Content-Type: application/json' \
-  -H 'cache-control: no-cache' \
-  -d '{
-	"name" : "new_team_3",
-	"Description" : "brand new team"
-}'
-
-
-#subscribe to team
+#subscribe to team (jdoe_1)
+for i in {1..200}
+do
 curl -X POST \
-  http://localhost:8000/api/team/new_team/subscribe \
-  -H 'Authorization: Basic amRvZUBlbWFpbC5jb206YQ==' \
+  http://${HOST}/api/team/new_team_${i}/subscribe \
+  -H 'Authorization: Basic amRvZV8xQGVtYWlsLmNvbTph' \
   -H 'cache-control: no-cache'
+done
+
+#subscribe to team (jdoe_2)
+for i in {100..300}
+do
+curl -X POST \
+  http://${HOST}/api/team/new_team_${i}/subscribe \
+  -H 'Authorization: Basic amRvZV8yQGVtYWlsLmNvbTph' \
+  -H 'cache-control: no-cache'
+done
+
+#subscribe to team (jdoe_3)
+for i in {1..400}
+do
+curl -X POST \
+  http://${HOST}/api/team/new_team_${i}/subscribe \
+  -H 'Authorization: Basic amRvZV8zQGVtYWlsLmNvbTph' \
+  -H 'cache-control: no-cache'
+done
